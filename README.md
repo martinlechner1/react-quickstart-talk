2abba23937deb9f5f769a5568a04d4f84925f3a6
martinlechner1 <martin.lechner.ml@gmail.com>
2017-06-05 23:00:48 +0200

    First draft for the outline.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-quickstart-talk
-Slides and demo code I'll be using for a quickstart to reactjs talk.
+Slides and demo code I'll be using for a quickstart to reactjs talk. 
+
+The slides were created with [spectacle from formidable labs](https://github.com/FormidableLabs/spectacle). To start the presentation go to the slides folder and run `yarn start`.
--- slides/presentation/index.js
+++ slides/presentation/index.js
@@ -1,49 +1,51 @@
 // Import React
-import React from 'react';
+import React from "react";
 
 // Import Spectacle Core tags
 import {
-  BlockQuote,
-  Cite,
+  //BlockQuote,
+  //Cite,
   Deck,
   Heading,
   ListItem,
   List,
-  Quote,
+  //Quote,
   Slide,
   Text,
-  CodePane,
-} from 'spectacle';
+  //CodePane,
+  Link,
+  S
+} from "spectacle";
 
 // Import image preloader util
-import preloader from 'spectacle/lib/utils/preloader';
+import preloader from "spectacle/lib/utils/preloader";
 
 // Import theme
-import createTheme from 'spectacle/lib/themes/default';
+import createTheme from "spectacle/lib/themes/default";
 
 // Require CSS
-require('normalize.css');
-require('spectacle/lib/themes/default/index.css');
+require("normalize.css");
+require("spectacle/lib/themes/default/index.css");
 
 const images = {
-  city: require('../assets/city.jpg'),
-  kat: require('../assets/kat.png'),
-  logo: require('../assets/formidable-logo.svg'),
-  markdown: require('../assets/markdown.png'),
+  city: require("../assets/city.jpg"),
+  kat: require("../assets/kat.png"),
+  logo: require("../assets/formidable-logo.svg"),
+  markdown: require("../assets/markdown.png")
 };
 
 preloader(images);
 
 const theme = createTheme(
   {
-    primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quartenary: '#CECECE',
+    primary: "white",
+    secondary: "#1F2022",
+    tertiary: "#f07e07",
+    quartenary: "#CECECE"
   },
   {
-    primary: 'Montserrat',
-    secondary: 'Helvetica',
+    primary: "Montserrat",
+    secondary: "Helvetica"
   }
 );
 
@@ -51,18 +53,61 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck
-        transition={['zoom', 'slide']}
+        transition={["zoom", "slide"]}
         transitionDuration={500}
         theme={theme}
       >
-        <Slide transition={['zoom']} bgColor="primary">
+        <Slide transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+            React <S type="strikethrough">Native</S> Quickstart
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text margin="10px 0 0" textColor="tertiary" size={1} bold>
+            Martin Lechner - @martinlechner1
           </Text>
         </Slide>
+        <Slide>
+          <Heading size={6}>JSX</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={6}>Components</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={6}>Props</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={6}>Class Components</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={6}>State</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={6}>Lifecycle</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={6}>Architectural Considerations</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={6}>Higher Order Components</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={6}>Pain of modern JS</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={6}>Create React App ftw!</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={6}>Pain of State Management</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={6}>Redux</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={6}>React-Redux</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={6}>React Native</Heading>
+        </Slide>
+        {/*
         <Slide transition={['fade']} bgColor="tertiary">
           <Heading size={6} textColor="primary" caps>Typography</Heading>
           <Heading size={1} textColor="secondary">Heading 1</Heading>
@@ -93,6 +138,32 @@ export default class Presentation extends React.Component {
               'const Button = ({ title }) => (<button type="button">{ title }</button>);'
             }
           </CodePane>
+        </Slide>*/}
+        <Slide>
+          <Heading size={5} textColor="tertiary">
+            Links
+          </Heading>
+          <List size={6}>
+            <ListItem>
+              <Link href="https://facebook.github.io/react">React</Link>
+            </ListItem>
+            <ListItem>
+              <Link href="https://github.com/facebookincubator/create-react-app">
+                Create-React-App
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link href="http://redux.js.org/">
+                Redux
+              </Link>
+            </ListItem>
+          </List>
+        </Slide>
+        <Slide>
+          <Heading size={5} textColor="tertiary">
+            Slides and Demo code
+          </Heading>
+          <Text>https://github.com/martinlechner1/react-quickstart-talk</Text>
         </Slide>
       </Deck>
     );
