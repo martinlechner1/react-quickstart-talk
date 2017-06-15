37b71663fc647a5a8ed8b928e50bbb2ceac180c7
andys8 <andys8@users.noreply.github.com>
2017-06-15 15:39:25 +0200

    Slides: Removed comments

--- slides/presentation/index.js
+++ slides/presentation/index.js
@@ -264,39 +264,6 @@ export default class Presentation extends React.Component {
             <ListItem>Andy S.</ListItem>
           </List>
         </Slide>
-
-        {/*
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <CodePane lang="jsx">
-            {
-              'const Button = ({ title }) => (<button type="button">{ title }</button>);'
-            }
-          </CodePane>
-        </Slide>*/}
         <Slide>
           <Heading size={5} textColor="tertiary">
             Links
